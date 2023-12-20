2de434887777d4842d38f13a7db2299117cbd9ab
pamudu-ratnayake <it19993416@my.sliit.lk>
2023-12-20 17:40:30 +0530

    add swagger doc

--- backend/src/routes/userRT.js
+++ backend/src/routes/userRT.js
@@ -11,4 +11,145 @@ router.get("/getFavoriteCoins",auth, getFavoriteCoins);
 router.put("/addToFavorite",auth, addFavoriteCoin);
 router.delete("/removeFavorite",auth, removeFavoriteCoin);
 
+/**
+ * @swagger
+ * /auth-user/signup:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+
+* /auth-user/login:
+ *   post:
+ *     summary: Login user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+
+* /auth-user/get-user:
+ *   get:
+ *     summary: Get user Details
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+
+* /auth-user/getFavoriteCoins:
+ *   get:
+ *     summary: Get user favorite coins
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+
+* /auth-user/addToFavorite:
+ *   put:
+ *     summary: Add user favorite coins
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                coin:
+ *                  type: object
+ *                  properties:
+ *                       id:
+ *                        type: string
+ *                       name:
+ *                        type: string
+ *                       symbol:
+ *                        type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+
+* /auth-user/removeFavorite:
+ *   delete:
+ *     summary: Remove user favorite coins
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                coin:
+ *                  type: object
+ *                  properties:
+ *                       id:
+ *                        type: string
+ *                       name:
+ *                        type: string
+ *                       symbol:
+ *                        type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 require("dotenv").config();
 const app = express();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const PORT = process.env.PORT || 8070;
 
@@ -29,6 +31,22 @@ connection.once("open", () => {
 app.listen(PORT, () => {
     console.log(`Server is up and running on port : ${PORT}`);
 });
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Cryptocurrency Price Tracking",
+            version: "1.0.0",
+            description: "Cryptocurrency Price Tracking for spera labs test",
+        },
+    },
+    apis: ["./src/routes/*.js"], // Path to the API routes
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 const authRoutes = require("../src/routes/userRT");
 app.use("/auth-user", authRoutes);
